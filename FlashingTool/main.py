078a614246ddbafceb257c19e88cae065c3cb813
anuarrozman <anuar.rozman@airdroitech.com>
2024-10-01 10:55:23 +0800

    Test fix bind keys

--- FlashingTool/main.py
+++ FlashingTool/main.py
@@ -112,6 +112,8 @@ class SerialCommunicationApp:
         self.factory_flag = None
         self.used_cert_ids = set()
         self.selected_cert_id = None
+        self.current_set = None
+        self.button_sets = []
 
         #self.configure_logging()
 
